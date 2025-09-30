131039fadcd0ab70588e0e134e6614576fd3cab1
Muniira243 <mumoh7555@oslomet.no>
2025-09-30 14:13:38 +0200

    takeQiozController

--- QuizApp/Controllers/TakequizController.cs
+++ QuizApp/Controllers/TakequizController.cs
@@ -1,56 +1,70 @@
-using QuizApp.Models;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyShop.Models;
  
- 
- 
-public class TakeQuizController : Controller
+namespace MyShop.Controllers
 {
-    private readonly QuizDbContext_context;
- 
-    public TakeQuizController(QuizDbContext context)
+    public class TakeQuizController : Controller
     {
-        QuizDbContext_context = context;
-    }
+        private readonly ItemDbContext _context;
  
-    // Show quiz questions
-    public IActionResult Take(int quizId)
-    {
-        var quiz = _context.Quizzes
-                    .Include(q => q.Questions)
-                    .ThenInclude(q => q.AnswerOptions)
-                    .FirstOrDefault(q => q.QuizId == quizId);
- 
-        if (quiz == null) return NotFound();
-        return View(quiz);
-    }
- 
-    [HttpPost]
-    public IActionResult Submit(int quizId, Dictionary<int, int> answers, string userName)
-    {
-        var quiz = _context.Quizzes
-                    .Include(q => q.Questions)
-                    .ThenInclude(q => q.AnswerOptions)
-                    .FirstOrDefault(q => q.QuizId == quizId);
- 
-        int score = 0;
-        foreach (var question in quiz.Questions)
+        public TakeQuizController(ItemDbContext context)
         {
-            if (answers.TryGetValue(question.QuestionId, out int selectedAnswerId))
-            {
-                var selectedOption = question.AnswerOptions.FirstOrDefault(a => a.AnswerOptionId == selectedAnswerId);
-                if (selectedOption != null && selectedOption.IsCorrect) score++;
-            }
+            _context = context;
         }
  
-        _context.UserQuizResults.Add(new UserQuizResult { QuizId = quizId, UserName = userName, Score = score });
-        _context.SaveChanges();
+        // GET: /TakeQuiz/Take/5
+        [HttpGet]
+        public async Task<IActionResult> Take(int id)
+        {
+            var quiz = await _context.Quizzes
+                .Include(q => q.Questions)
+                .ThenInclude(q => q.AnswerOptions)
+                .FirstOrDefaultAsync(q => q.QuizId == id);
  
-        return RedirectToAction("Result", new { quizId = quizId, userName = userName });
-    }
+            if (quiz == null) return NotFound();
  
-    public IActionResult Result(int quizId, string userName)
-    {
-        var result = _context.UserQuizResults.FirstOrDefault(r => r.QuizId == quizId && r.UserName == userName);
-        return View(result);
+            return View(quiz);
+        }
+ 
+        // POST: /TakeQuiz/Submit
+        [HttpPost]
+        public async Task<IActionResult> Submit(int quizId, string userName, Dictionary<int, int> answers)
+        {
+            var quiz = await _context.Quizzes
+                .Include(q => q.Questions)
+                .ThenInclude(q => q.AnswerOptions)
+                .FirstOrDefaultAsync(q => q.QuizId == quizId);
+ 
+            if (quiz == null) return NotFound();
+ 
+            int score = 0;
+            foreach (var q in quiz.Questions)
+            {
+                if (answers.ContainsKey(q.QuestionId))
+                {
+                    var selectedOption = q.AnswerOptions.FirstOrDefault(o => o.AnswerOptionId == answers[q.QuestionId]);
+                    if (selectedOption != null && selectedOption.IsCorrect)
+                    {
+                        score++;
+                    }
+                }
+            }
+ 
+            var result = new UserQuizResult
+            {
+                UserName = userName,
+                Quiz = quiz,
+                Score = score
+            };
+ 
+            return View("Result", result);
+        }
+ 
+        // GET: /TakeQuiz/Result (kun for redirecting)
+        [HttpGet]
+        public IActionResult Result(UserQuizResult result)
+        {
+            return View(result);
+        }
     }
 }
--- QuizApp/obj/Debug/net8.0/QuizApp.AssemblyInfo.cs
+++ QuizApp/obj/Debug/net8.0/QuizApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuizApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19d0131988f7a364b25666e76153a6a349a2fb2d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8dce958c2b6617951d23124a58fb32904a91e11d")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuizApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuizApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
